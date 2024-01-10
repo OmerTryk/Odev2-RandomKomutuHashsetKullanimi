c15ad02f81be4cb7b6064be1dac172b718876c0b
OmerTryk <omertryk69@gmail.com>
2024-01-10 18:48:36 +0300

    Add project files.

---
+++ Ödev2-RandomKomutu/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Başlangıç değeri: ");
+        int baslangic = int.Parse(Console.ReadLine());
+
+        Console.Write("Bitiş değeri: ");
+        int bitis = int.Parse(Console.ReadLine());
+
+        Console.Write("Oluşturulacak sayı adedi: ");
+        int sayiAdedi = int.Parse(Console.ReadLine());
+
+        Random rnd = new Random();
+        HashSet<int> essizsayilar = new HashSet<int>();
+
+        while (essizsayilar.Count < sayiAdedi)
+        {
+            int rasgeleSayi = rnd.Next(baslangic, bitis + 1);
+            essizsayilar.Add(rasgeleSayi);
+        }
+
+        int[] rasgeleSayilar = essizsayilar.ToArray();
+
+        Array.Sort(rasgeleSayilar);
+
+        for (int i = 0; i <rasgeleSayilar.Length; i++)
+        {
+            Console.WriteLine(rasgeleSayilar[i]);
+        }
+
+        Console.WriteLine();
+
+    }
+}
